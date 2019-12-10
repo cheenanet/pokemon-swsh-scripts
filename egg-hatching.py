9c2e0d8e482e0b6b80eb6e89e4bbdd44b2e7260b
Cheena <cheena@cheena.net>
2019-12-10 17:21:38 +0000

    Add scripts

---
+++ egg-hatching.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python3
+import argparse
+import serial
+import time
+from time import sleep
+import datetime
+
+parser = argparse.ArgumentParser()
+parser.add_argument('port')
+args = parser.parse_args()
+
+datetime = datetime.datetime
+
+def send(msg, duration=0):
+    now = datetime.now()
+    print(f'[{now}] {msg}')
+    ser.write(f'{msg}\r\n'.encode('utf-8'))
+    sleep(duration)
+    ser.write(b'RELEASE\r\n')
+
+ser = serial.Serial(args.port, 9600)
+
+# そらをとぶ
+# タマゴもらう
+#
+#
+#
+#
+#
+
+# サイクル20 = 30
+laps = 20 # 10000 = 60?
+
+try:
+    # テスト
+    send('HAT CENTER', 3)
+
+    for i in range(0, 6): # 5匹孵化×6回
+        start_time = time.time()
+
+        for j in range(0, 5): # 1匹孵化×5
+            lap_start_time = time.time()
+
+            send('Button X', 0.1)
+            sleep(1)
+
+            send('Button A', 0.1)
+            sleep(2.5)
+
+            send('Button A', 0.1)
+            sleep(0.5)
+
+            send('Button A', 0.1)
+            sleep(2)
+
+            # タマゴもらう
+            send('LY MAX', 0.7)
+            send('LX MAX', 0.2)
+            sleep(0.5)
+
+            send('Button A', 0.1) # キミのポケモンが…
+            sleep(0.5)
+
+            send('Button A', 0.1) # 欲しいですよね
+            sleep(0.5)
+
+            send('Button A', 0.1) # はい
+            sleep(3)
+
+            send('Button A', 0.1) # 預け屋さんからタマゴをもらった
+            sleep(3)
+
+            send('Button A', 0.1) # 手持ちに加えました
+            sleep(2)
+
+            send('Button A', 0.1) # 大事に育ててね
+            sleep(0.1)
+
+            send('LY MIN', 3)
+            sleep(0.1)
+
+            send('LX MAX', 2)
+            sleep(0.1)
+
+            for lap in range(0, laps):
+                print(f'{lap + 1}周目')
+
+                send('LY MIN', 0.5)
+
+                send('LX MIN', 0.5)
+
+                send('LY MAX', 0.5)
+
+                send('LX MAX', 0.5)
+
+                send('Button B', 0.3)
+
+            # ボタン押せるようになるまで待機
+            for wait in range(0, 8):
+                send('Button B', 0.1)
+                sleep(1)
+
+            print(f'{j + 1}匹孵化完了？：{round(time.time() - lap_start_time, 2)}秒経過（合計：{round(time.time() - start_time, 2)}秒）')
+
+        for lap in range(0, laps):
+            print(f'{lap + 1}周目')
+
+            send('LY MIN', 0.5)
+            send('LX MIN', 0.5)
+            send('LY MAX', 0.5)
+            send('LX MAX', 0.5)
+
+            send('Button B', 0.3)
+
+        # ボタン押せるようになるまで待機
+        for wait in range(0, 8):
+            send('Button B', 0.1)
+            sleep(1)
+
+        print(f'5匹孵化完了：{round(time.time() - start_time, 2)}秒経過')
+
+        send('Button X', 0.1)
+        sleep(1)
+
+        send('LX MAX', 0.1)
+        sleep(0.1)
+
+        send('LY MIN', 0.1)
+        sleep(0.1)
+
+        send('Button A', 0.1) # ポケモン
+        sleep(2)
+
+        send('Button R', 0.1) # ボックスを開く
+        sleep(2)
+
+        send('LX MIN', 0.1)
+        sleep(0.1)
+
+        send('LY MAX', 0.1) # タマゴにカーソル
+        sleep(0.1)
+
+        send('Button Y', 0.1)
+        sleep(0.1)
+
+        send('Button Y', 0.1) # はんいモード
+        sleep(0.1)
+
+        send('Button A', 0.1)
+        sleep(0.1)
+
+        send('LY MAX', 0.1)
+        sleep(0.1)
+
+        send('LY MAX', 0.1)
+        sleep(0.1)
+
+        send('LY MAX', 0.1)
+        sleep(0.1)
+
+        send('LY MAX', 0.1)
+        sleep(0.1)
+
+        send('Button A', 0.1) # タマゴ全選択
+        sleep(0.1)
+
+        send('LX MAX', 0.1)
+        sleep(0.1)
+
+        send('LY MAX', 0.1)
+        sleep(0.1)
+
+        send('LY MAX', 0.1)
+        sleep(0.1)
+
+        send('LY MAX', 0.1)
+        sleep(0.1)
+
+        send('LY MAX', 0.1)
+        sleep(0.1)
+
+        send('Button A', 0.1) # いちらん
+        sleep(0.5)
+
+        send('Button A', 0.1) # ボックスに置く
+        sleep(0.5)
+
+        send('Button B', 0.1) # もどる
+        sleep(0.1)
+
+        send('Button B', 0.1) # もどる
+        sleep(2)
+
+        send('Button B', 0.1) # もどる
+        sleep(1.5)
+
+        send('LY MAX', 0.1)
+        sleep(0.1)
+
+        send('LX MIN', 0.1)
+        sleep(0.1)
+
+        send('Button B', 0.1) # もどる
+        sleep(1.5)
+
+except KeyboardInterrupt:
+    send('RELEASE')
+    ser.close()
+
---
+++ tournament-battle.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+import argparse
+import serial
+import time
+from time import sleep
+import datetime
+import random
+
+parser = argparse.ArgumentParser()
+parser.add_argument('port')
+args = parser.parse_args()
+
+datetime = datetime.datetime
+
+def send(msg, duration=0):
+    now = datetime.now()
+    print(f'[{now}] {msg}')
+    ser.write(f'{msg}\r\n'.encode('utf-8'))
+    sleep(duration)
+    ser.write(b'RELEASE\r\n')
+
+ser = serial.Serial(args.port, 9600)
+
+# 試合時間
+fight_time = 60 * 2 + 15
+
+try:
+
+    # テスト
+    send('Button B', 3)
+
+    start_time = time.time()
+
+    # トーナメント自動化
+
+    for lap in range(0, 999):
+        send('Button A', 0.2) # 話しかける
+        sleep(1)
+
+        send('Button A', 0.1) 
+        sleep(0.5)
+
+        send('Button A', 0.1) # はい
+        sleep(1)
+
+        send('Button A', 0.1) 
+        sleep(0.5)
+
+        send('Button A', 0.1) # いる
+        sleep(0.5)
+
+        send('Button A', 0.1)
+        sleep(0.5)
+
+        send('LY MAX', 0.1)
+        sleep(0.1)
+
+        send('LY MAX', 0.1) # ホップ
+        sleep(0.1)
+
+        for i in range(0, 13):
+            send('Button A', 0.1)
+            sleep(0.4)
+
+        # 入場
+
+        for i in range(0, 3):
+            send('LY MIN', 3) # 入場する
+            sleep(6)
+
+            send('Button A', 0.1) # セリフ1
+            sleep(1)
+
+            send('Button A', 0.1) # セリフ2
+            sleep(1)
+
+            send('Button A', 0.1) # セリフ3
+            sleep(13)
+
+            # 勝負
+
+            send('Button A', 0.1) # 勝負を　しかけてきた！
+            sleep(22)
+
+            # スペシャルアップを使う
+            send('LY MAX', 0.1)
+            sleep(0.1)
+
+            send('LY MAX', 0.1)
+            sleep(0.1)
+
+            send('Button A', 0.1)
+            sleep(1.5)
+
+            send('LX MAX', 0.1)
+            sleep(0.1)
+
+            send('LX MAX', 0.1)
+            sleep(0.1)
+
+            send('LY MAX', 0.1)
+            sleep(0.1)
+
+            send('LY MAX', 0.1)
+            sleep(0.1)
+
+            send('Button A', 0.1)
+            sleep(0.2)
+
+            send('Button A', 0.1)
+            sleep(12)
+
+            send('LY MIN', 0.1)
+            sleep(0.1)
+
+            send('LY MIN', 0.1)
+            sleep(0.1)
+
+            fight_start_time = time.time()
+
+            while True:
+                if (time.time() - fight_start_time) > fight_time:
+                    break
+
+                send('Button A', 0.1)
+                sleep(0.1)
+
+                if random.randrange(2):
+                    time_left = round(fight_time - (time.time() - fight_start_time), 2)
+                    print(f'[{datetime.now()}] 残り{time_left}秒')
+
+            print('勝利')
+
+        # 優勝
+        print('優勝')
+
+        # ボールガイ
+        for i in range(0, 20):
+            send('Button A', 0.1)
+            sleep(1)
+
+        send('LY MIN', 1) # 受付に突っ込む
+
+        print(f'[{datetime.now()}] {round(time.time() - start_time, 2)}秒経過（{lap}回目）')
+
+except KeyboardInterrupt:
+    send('RELEASE')
+    ser.close()
