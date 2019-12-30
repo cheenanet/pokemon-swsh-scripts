c7e74a045c4dc40a9f1f93d77399cedd73dd8f69
Cheena <cheena@cheena.net>
2019-12-30 21:25:30 +0000

    Allow to specify a maximum number of releases

--- release.py
+++ release.py
@@ -7,8 +7,7 @@ import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument('port')
-parser.add_argument('--rows', type=int, default=5)
-parser.add_argument('--cols', type=int, default=6)
+parser.add_argument('--count', type=int)
 args = parser.parse_args()
 
 def send(msg, duration=0):
@@ -21,37 +20,39 @@ def send(msg, duration=0):
 ser = serial.Serial(args.port, 9600)
 
 try:
-    for row in range(0, args.rows):
-        for col in range(0, args.cols):
-            send('Button A', 0.1)
-            sleep(0.2)
+    for i in range(1, args.count):
+        print(f'{i}匹目')
 
-            send('LY MIN', 0.1)
-            sleep(0.1)
+        send('Button A', 0.1)
+        sleep(0.2)
 
-            send('LY MIN', 0.1)
-            sleep(0.1)
-
-            send('Button A', 0.1) # にがす
-            sleep(1)
-
-            send('LY MIN', 0.1)
-            sleep(0.1)
-
-            send('Button A', 0.1)
-            sleep(1.4)
-
-            send('Button A', 0.1)
-            sleep(0.2)
-
-            send('LX MAX', 0.1) # 次へ
-            sleep(0.2)
-
-        send('LX MAX', 0.1) # てもちへ
+        send('LY MIN', 0.1)
         sleep(0.1)
 
-        send('LY MAX', 0.1) # 下の行へ
+        send('LY MIN', 0.1)
         sleep(0.1)
+
+        send('Button A', 0.1) # にがす
+        sleep(1)
+
+        send('LY MIN', 0.1)
+        sleep(0.1)
+
+        send('Button A', 0.1)
+        sleep(1.4)
+
+        send('Button A', 0.1)
+        sleep(0.2)
+
+        send('LX MAX', 0.1) # 次へ
+        sleep(0.2)
+
+        if i % 6 == 0:
+            send('LX MAX', 0.1) # てもちへ
+            sleep(0.1)
+
+            send('LY MAX', 0.1) # 下の行へ
+            sleep(0.1)
 
 except KeyboardInterrupt:
     send('RELEASE')
